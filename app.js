6f622722d089eb1389769dfce98c56fb321574c1
Estefani <stef45689@gmail.com>
2021-08-13 10:42:59 -0500

    HTML,CSS, JS Added

--- app.js
+++ app.js
@@ -1,8 +1,83 @@
-// import functions and grab DOM elements
+import {add} from './utils.js'; 
+import {subtract} from './utils.js'; 
+import {multiply} from './utils.js'; 
+import {divide} from './utils.js'; 
 
+// import functions and grab DOM elements
+const addInputEl1 = document.getElementById ('input-add1'); 
+const addInputEl2 = document.getElementById('input-add2'); 
+const addButtonEl = document.getElementById('add-button'); 
+const addResultEl = document.getElementById('add-result'); 
+
+const subtractInputEl1 = document.getElementById ('input-subtract1'); 
+const subtractInputEl2 = document.getElementById('input-subtract2'); 
+const subtractButtonEl = document.getElementById('subtract-button'); 
+const subtractResultEl = document.getElementById('subtract-result'); 
+
+const multiplyInputEl1 = document.getElementById ('input-multiply1'); 
+const multiplyInputEl2 = document.getElementById('input-multiply2'); 
+const multiplyButtonEl = document.getElementById('multiply-button'); 
+const multiplyResultEl = document.getElementById('multiply-result'); 
+
+const divideInputEl1 = document.getElementById ('input-divide1'); 
+const divideInputEl2 = document.getElementById('input-divide2'); 
+const divideButtonEl = document.getElementById('divide-button'); 
+const divideResultEl = document.getElementById('divide-result'); 
 // initialize state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+addButtonEl.addEventListener('click', ()=>{
+  const num1= Number(addInputEl1.value); 
+  const num2 = Number(addInputEl2.value);
+
+  const sum = add(num1, num2);
+
+  addResultEl.textContent = sum;
+});
+
+subtractButtonEl.addEventListener('click', ()=>{
+  const num1 = Number(subtractInputEl1.value); 
+  const num2 = Number(subtractInputEl2.value);
+
+  const sum = subtract(num1, num2);
+
+  subtractResultEl.textContent = sum;
+});
+
+multiplyButtonEl.addEventListener('click', ()=>{
+  const num1 = Number(multiplyInputEl1.value); 
+  const num2 = Number(multiplyInputEl2.value);
+
+  const sum = multiply(num1, num2);
+
+  multiplyResultEl.textContent = sum;
+});
+
+divideButtonEl.addEventListener('click', ()=>{
+  const num1 = Number(divideInputEl1.value); 
+  const num2= Number(divideInputEl2.value);
+
+  const sum = divide(num1, num2);
+
+  divideResultEl.textContent = sum;
+});
+
+/*function add(num1,num2){
+  return num1 + num2;
+}
+
+function subtract(num1,num2){
+  return num1 - num2;
+}
+
+function multiply(num1,num2){
+  return num1 * num2;
+}
+
+function divide(num1,num2){
+  return num1 / num2;
+}
+*/
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,31 @@
 import './example.test.js';
+import {add} from './utils.js'; 
+import {subtract} from './utils.js'; 
+import {multiply} from'./utils.js'; 
+import {divide} from './utils.js'; 
+
+const test = QUnit.test;
+
+test('should take in 1 and 1 and return 2', (expect) =>{
+    const expected = 2; 
+    const actual = add(1,1); 
+    expect.equal(actual,expected); 
+});
+
+test('should take in 1 and 1 and return 0', (expect) =>{
+    const expected = 0; 
+    const actual = subtract(1,1); 
+    expect.equal(actual,expected); 
+});
+
+test('should take in 2 and 4 and return 8', (expect) =>{
+    const expected = 8; 
+    const actual = multiply(2,4); 
+    expect.equal(actual,expected); 
+});
+
+test('should take in 4 and 2 and return 2', (expect) =>{
+    const expected = 2; 
+    const actual = divide(4,2); 
+    expect.equal(actual,expected); 
+});
